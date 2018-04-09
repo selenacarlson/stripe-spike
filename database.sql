4287b21d8d900df6c39f41f4ecd0aef065dedf1d
Selena Carlson <selena.h.carlson@gmail.com>
2018-04-09 14:56:09 -0500

    added table to sql file

--- database.sql
+++ database.sql
@@ -7,3 +7,17 @@ CREATE TABLE users
     customer_id character varying
 );
 
+CREATE TABLE nonprofits
+(
+    id SERIAL PRIMARY KEY,
+    name character varying,
+    city character varying,
+    state character varying,
+    description character varying,
+    product_id character varying,
+    plan_id_five character varying,
+    plan_id_ten character varying,
+    plan_id_twenty character varying,
+    created Date
+);
+
